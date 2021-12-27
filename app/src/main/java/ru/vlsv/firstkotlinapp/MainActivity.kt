ff99dbbed71fd3ba0afaf8cb370addf74d3751b6
Anatoly Lebedev <44133485+CentnerMan@users.noreply.github.com>
2021-12-27 17:41:44 +0300

    First strings

--- app/src/main/java/ru/vlsv/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/ru/vlsv/firstkotlinapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package ru.vlsv.firstkotlinapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mMainTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mMainTextView = findViewById(R.id.txt_main)
+
+        var mainBtn = findViewById<Button>(R.id.btn_main)
+
+        mainBtn.setOnClickListener {
+            mMainTextView.setText(getString(R.string.press_main_btn))
+        }
     }
 }
